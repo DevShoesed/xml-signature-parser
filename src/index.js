4ba33a78a50a47cae9a023897b5dd6f74170359e
DevShoesed <francesco.scarpato@gmail.com>
2021-07-22 21:31:33 +0200

    Add write of XLSX file

--- src/index.js
+++ src/index.js
@@ -13,17 +13,55 @@ const options = yargs.usage('Usage: -d <directory>').option('d', {
 }).argv
 
 try {
-  let workbook = new Excel.Workbook()
-  let worksheet = workbook.addWorksheet('Segnatues')
+  const workbook = new Excel.Workbook()
+  const worksheet = workbook.addWorksheet('Segnatues')
+
+  worksheet.columns = [
+    { header: 'Id', key: 'id', width: 10 },
+    { header: 'Numero', key: 'numero', width: 32 },
+    { header: 'Anno', key: 'anno', width: 32 },
+  ]
 
   const subdirs = fse.readdirSync(options.directory)
-  subdirs.forEach((file) => {
+
+  subdirs.forEach(async (file) => {
     const segnaturaObj = parseFile.getSegnaturaObj(
       `${options.directory}/${file}/Segnatura.xml`
     )
 
-    worksheet.addRow({})
+    const row = {
+      id: file,
+      numero:
+        segnaturaObj.Segnatura.Intestazione.Identificatore.NumeroRegistrazione,
+      anno: new Date(
+        segnaturaObj.Segnatura.Intestazione.Identificatore.DataRegistrazione
+      ).getFullYear(),
+    }
+
+    if (segnaturaObj.Segnatura.Descrizione.Allegati) {
+      //console.log(segnaturaObj.Segnatura.Descrizione.Allegati)
+      Object(segnaturaObj.Segnatura.Descrizione.Allegati).forEach((all) => {
+        console.log(all)
+      })
+      // var i = 1
+      // segnaturaObj.Segnatura.Descrizione.Allegati.forEach(async (all) => {
+      //   //row[`allegato${i}`] = all.
+      //   console.log(all)
+      //   i++
+      // })
+    }
+
+    await worksheet.addRow(row)
   })
+
+  await workbook.xlsx
+    .writeFile('export.xlsx')
+    .then(() => {
+      console.log('saved')
+    })
+    .catch((err) => {
+      console.log('err', err)
+    })
 } catch (error) {
   console.error(error.message)
 }
